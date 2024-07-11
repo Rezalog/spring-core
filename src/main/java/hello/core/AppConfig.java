1dd40e0ea0e795519b4a7cac59544d6cb4d26cec
Rezal <rladlfdnd369@gmail.com>
2024-07-11 13:09:31 +0900

    AppConfig refactoring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -10,10 +10,19 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() { // 호출 시 MemeberServiceImpl 생성자 생성시 MemoryMemeberRepository와 의존관계 주입 및 연결
-        return new MemberServiceImpl(new MemoryMemberRepository());
+//        return new MemberServiceImpl(new MemoryMemberRepository());/**/
+        return new MemberServiceImpl(memberRepository()); // refactoring - method 분리(ctrl alt M)
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() { // 호출 시 OrderServiceImpl의 생성자 생성, 구체클래스인 MemoryMemeber 및 FoxPolicy 의존성 주입
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private static FixDiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
