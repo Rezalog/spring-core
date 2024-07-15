44d8d5daec16edf11f95ccc9a1129cc718803eba
Rezal <rladlfdnd369@gmail.com>
2024-07-15 16:13:51 +0900

    [5] Singleton End

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -17,9 +17,15 @@ import org.springframework.context.annotation.Configuration;
  * 이렇게 출력되어야하지만(순서미보장), 실제로 memberService, memberRepository, orderService 만 출력된다.
  *
  * 이는 @Configuration 이 적용되지않은 순수 자바의 DI Container와
- * @Configuration 이 적용된 Spring DI Container 의 차이로
- * Spring DI Container 에서는 Bean 등록시 바이트 코드를 이용한 클래스 재명명으로 등록함으로서
- * 반드시 싱글톤을 보장해줌을 알 수 있다.
+ * @Configuration 이 적용된 Spring DI Container 의 차이로,
+ * Spring DI Container 에서는 Bean 등록시 스프링 컨테이너에 존재하면
+ * 스프링 컨테이너에서 찾아 반환하거나, 없으면 새로 바이트 코드를 이용한 cglib을 적용하여
+ * 대상 클래스를 상속받는 @cglib$xxx class로 바꾸어 Spring Container에 동적으로 등록한 것을 반환한다.
+ *
+ * 이 덕분에 싱글톤을 보장함을 알 수 있다.
+ *
+ * 하지만 @Configuration 없이 @Bean 으로만 등록하면 cglib 이 적용되지않고,
+ * Spring DI Container에 등록은 되지만 싱글톤을 보장할 수 없다.
  *
  * */
 
--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -1,6 +1,10 @@
 package hello.core.singleton;
 
 import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,6 +14,28 @@ public class ConfigurationSingletonTest {
     @Test
     void configurationTest() {
         ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+        System.out.println("memberService = " + memberService);
+        System.out.println("orderService = " + orderService);
+        System.out.println("memberRepository = " + memberRepository);
+    }
+    /*
+    *   call AppConfig.memberService
+        call AppConfig.memberRepository
+        call AppConfig.orderService
+        call AppConfig.discountPolicy
+        memberService = hello.core.member.MemberServiceImpl@1b822fcc
+        orderService = hello.core.order.OrderServiceImpl@24a1c17f
+        memberRepository = hello.core.member.MemoryMemberRepository@56102e1c
+    * */
 
+    @Test
+    void configurationDeepTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+        System.out.println("bean.getClass() = " + bean.getClass());
+        // bean.getClass() = class hello.core.AppConfig$$SpringCGLIB$$0
     }
 }
