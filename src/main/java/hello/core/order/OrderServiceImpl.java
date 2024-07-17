be9a6f65de20c29cf2bb28a56426b6b7b1d0de48
Rezal <rladlfdnd369@naver.com>
2024-07-17 10:31:55 +0900

    [7] DiscountPolicy ìㅠB조회한 읐 이 모모두 필요할 때 - 선택할 ì수 있을 때)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,8 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // final 로 붙은 필드 변수를 대상, 생성자 의존관계 주입 및 getter setter 등 자동 생성
-//@MainDiscountPolicy
+//@RequiredArgsConstructor // final 로 붙은 필드 변수를 대상, 생성자 의존관계 주입 및 getter setter 등 자동 생성
+@MainDiscountPolicy
 public class OrderServiceImpl implements OrderService {
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -23,10 +23,10 @@ public class OrderServiceImpl implements OrderService {
 
     // 외부의 AppConfig 에서 구체클래스 의존관계 직접 주입, OrderImpl은 주입 시 필요한 생성자 생성에만 관여
 //    @Autowired //: 생성자가 1개일 시 Autowired 자동 적용
-//    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
